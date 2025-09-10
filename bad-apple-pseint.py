07ecf8f6cd25d69e7d0e4bafaff61fd01a31e16b
xZpookyx <129822298+xZpookyx@users.noreply.github.com>
2025-09-09 19:55:11 -0600

    Add PSeInt boilerplate

--- bad-apple-pseint.py
+++ bad-apple-pseint.py
@@ -18,14 +18,43 @@ def main():
         #     print("Video is not a valid video file")
         #     continue
         
+        # TODO: Eng / Esp languages
+        language: int = 0
+        
         current_path: str = sys.path[0]
         output_path: str = os.path.join(current_path, f"output/{output_name}.psc")
-            
+        video_fps: int = 15
+        
+        # Generate file
         with open(output_path, "w") as file:
+            # Entry
+            entry_text: str
+            if language == 0:
+                entry_text = "Press a key to play Bad Apple"
+            else:
+                entry_text = "Presiona una tecla para reproducir Bad Apple"
             
+            file.write(
+                "Algoritmo bad_apple\n"
+                f"\tEscribir \"{entry_text}\";\n"
+                "\tEsperar Tecla;\n"
+                "\tplay_video;\n"
+                "FinAlgoritmo\n"
+                "\n"
+                )
             
-            file.write("Hi")
-            file.write("Hi2")
+            # Video frames
+            ms: float = 1000 / video_fps
+            file.write("SubProceso play_video\n")
+            
+            # for each line in frame
+            file.write("\tLimpiar Pantalla;\n")
+            video_line: str = "Placeholder"
+            file.write(f"\tEscribir \"{video_line}\";\n")
+            
+            file.write(f"\tEsperar {ms} Milisegundos;\n")
+            
+            file.write("FinSubProceso")
         
         sys.exit()
 
