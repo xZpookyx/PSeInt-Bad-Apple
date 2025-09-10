ce05da02bc41d5e219a797f17cdf4c11cae9b27c
xZpookyx <129822298+xZpookyx@users.noreply.github.com>
2025-09-09 22:46:15 -0600

    Add file generation settings

--- bad-apple-pseint.py
+++ bad-apple-pseint.py
@@ -14,36 +14,19 @@ def light_to_ascii(light: int) -> str:
     return ascii_chars[light]
     
 
-def main():
-    while True:
-        print("Create pseint file")
-        # output_name = input("Enter output file name: ")
-        
-        # video_path = input("Enter source video path: ")
-        
-        # if not os.path.exists(video_path):
-        #     print("Video does not exist")
-        #     continue
-        
-        # if not video_path.endswith(".mp4"):
-        #     print("Video is not a valid video file")
-        #     continue
-        
-        # TODO: Eng / Esp languages
-        language: int = 0
-        
-        current_path: str = sys.path[0]
-        video_path: str = os.path.join(current_path, "Bad apple.mp4")
-        output_path: str = os.path.join(current_path, f"output/bad_apple.psc")
-        video_fps: int = 1
-        resolution: tuple[int, int] = (70, int(70 / 3.5))
-        
-        # Generate file
-        with open(output_path, "w") as file:
+def generate_file(
+    resolution: tuple[int, int], 
+    video_fps: int,
+    video_path: str,
+    output_path: str,
+    language: int
+    ):
+    
+    with open(output_path, "w") as file:
             # Entry
             title_text: str 
             entry_text: str
-            if language == 0:
+            if language == 1:
                 title_text = "!! Rescale the screen until the box fits !!"
                 entry_text = "Press a key to play Bad Apple"
             else:
@@ -116,7 +99,7 @@ def main():
                     "FinSubProceso"
                 )
                 cap.release()
-                break
+                return
             
             file.write("SubProceso play_video\n")
             
@@ -143,6 +126,96 @@ def main():
                 file.write(f"\tEsperar {ms} Milisegundos;\n")
             
             file.write("FinSubProceso")
+
+def main():
+    while True:
+        print("Create pseint file")
+        while True:
+            video_path = input("Enter source video path: ")
+            
+            if not os.path.exists(video_path):
+                print("Video does not exist")
+                continue
+            
+            if not video_path.endswith(".mp4"):
+                print("Video is not a valid video file")
+                continue
+            break
+        print("")
+        
+        while True:
+            user_input = input("Enter video width (in characters): ")
+            
+            if not user_input.isdigit():
+                print("Not a number")
+                continue
+            
+            if int(user_input) <= 0:
+                print("Width must be greater than 0")
+                continue
+            width = int(user_input)
+            break
+        print("")
+        
+        while True:
+            user_input = input("Enter video fps: ")
+            
+            if not user_input.isdigit():
+                print("Not a number")
+                continue
+            
+            if int(user_input) <= 0:
+                print("Fps must be greater than 0")
+                continue
+            
+            video_fps = int(user_input)
+            break
+        print("")
+        
+        while True:
+            output_name = input("Enter output file name: ")
+            
+            if len(output_name) == 0:
+                print("Empty name not allowed")
+                continue
+            break
+        print("")
+        
+        while True:
+            print("1. English")
+            print("2. Spanish")
+            user_input = input("Select output language (1-2): ")
+            
+            if not user_input.isdigit():
+                print("Not a number")
+                continue
+            
+            if not int(user_input) in (1, 2):
+                print("Outside of range")
+                continue
+            
+            language = int(user_input)
+            break
+        print("")
+        
+        "Generating file..."
+        
+        current_path: str = sys.path[0]
+        output_path: str = os.path.join(current_path, f"output/{output_name}.psc")
+        resolution: tuple[int, int] = (width, int(width / 3.5))
+        
+        # video_path: str = os.path.join(current_path, "Bad apple.mp4")
+        # video_fps: int = 1
+        # resolution: tuple[int, int] = (70, int(70 / 3.5))
+        
+        # Generate file
+        generate_file(
+            resolution=resolution,
+            video_path=video_path,
+            output_path=output_path,
+            video_fps=video_fps,
+            language=language
+        )
         
         sys.exit()
 
